efa348230a50e0c925b60cdb1a79396311655053
sunilpokhrel1 <sunil073bex@ioepc.edu.np>
2022-02-22 18:45:24 +0545

    Add project files.

---
+++ KnockoutDemo/Interface/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnockoutDemo.Interface
+{
+    interface IProductRepository
+    {
+        IEnumerable<TblProductList> GetAll();
+        TblProductList Get(int id);
+        TblProductList Add(TblProductList item);
+        bool Update(TblProductList item);
+        bool Delete(int id);
+    }
+}
---
+++ KnockoutDemo/Repositories/ProductDBEntities.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace KnockoutDemo.Repositories
+{
+    internal class ProductDBEntities
+    {
+        internal IEnumerable<TblProductList> TblProductLists;
+
+        public ProductDBEntities()
+        {
+        }
+    }
+}
